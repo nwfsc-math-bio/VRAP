b9336c3003eacd778cb318cee8568f423e03394d
Eli Holmes <eli.holmes@noaa.gov>
2017-03-14 12:09:38 -0700

    added pathname var
    set for shiny app

--- R/SetOutFileNames.R
+++ R/SetOutFileNames.R
@@ -1,17 +1,18 @@
 # '*****  SetOutFileNames  ******
 # 'Used by GetOutFiles and also by GetCommandLine
 # '************************************************************************
-SetOutFileNames = function(BaseName, inputs){
-# NoExtensionName  'stores path and name, but not extension
-# Position         'position of final "\" in pathname
+SetOutFileNames = function(BaseName, inputs, PathName=NULL){
+# NoExtensionName  'stores path and name, but not extension; in VB, not R
+# Position         'position of final "\" in pathname; in VB, not R
 # PathName         'string containing the path (including last "\")
-# InFileNameOnly   'name (excl path) of input file
+# InFileNameOnly   'name (excl path) of input file; in VB, not R
     
     #'SET OUTPUT FILES
-    inputs$OutFileSum = paste(BaseName,".sum",sep="")
-    inputs$OutFileEsc = paste(BaseName,".esc",sep="")
-    inputs$OutFileByr = paste(BaseName,".byr",sep="")
-    inputs$OutFileLog = paste(BaseName,".log",sep="")
-    inputs$InFileCopy = paste(BaseName,".rav",sep="")
+    inputs$OutFileSum = paste(PathName,BaseName,".sum",sep="")
+    inputs$OutFileEsc = paste(PathName,BaseName,".esc",sep="")
+    inputs$OutFileByr = paste(PathName,BaseName,".byr",sep="")
+    inputs$OutFileLog = paste(PathName,BaseName,".log",sep="")
+    inputs$InFileCopy = paste(PathName,BaseName,".rav",sep="")
+    inputs$OutFileReport = paste(PathName,BaseName,".pdf",sep="")
     return(inputs)
 }

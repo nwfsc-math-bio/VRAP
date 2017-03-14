89bf41dfa05db2fe12ff57765fed1d0b5cd847fb
Eli Holmes <eli.holmes@noaa.gov>
2017-03-14 12:07:19 -0700

    changed arguments
    added more so shiny app can call Main() with different inputs

--- R/Main.R
+++ R/Main.R
@@ -1,7 +1,13 @@
-Main = function(InFile=NULL, OutFileBase=NULL, NRuns=-1, silent=FALSE){
-  require(stringr)
+Main = function(InFile=NULL, OutFileBase=NULL, 
+                NRuns=-1, NYears=-1, Title=-1,
+                TargetStart=-1, TargetEnd=-1, TargetStep=-1,
+                ERecovery=-1, QET=-1, ECrit=-1, NewRavFileName="tmprav.rav",
+                silent=FALSE, lcores=1){
   
-  #if not called with input file, then user is prompted to input one
+  c1 = makeCluster(lcores)
+  registerDoParallel(c1)
+  
+  ## if not called with input file, then user is prompted to input one
   if(is.null(InFile)) InFile = file.choose()
   if(!file.exists(InFile)) stop("Specified input file does not exist.")
   if(is.null(OutFileBase)){
@@ -17,32 +23,54 @@ Main = function(InFile=NULL, OutFileBase=NULL, NRuns=-1, silent=FALSE){
   }
   
   
-#Two lists will be passed in and out of functions
-#   inputs = list() #is everything from the .rav file
-#   staticvars = list() #is anything computed from that; static
+  ## Two lists will be passed in and out of functions
+  ##    inputs = list() ## is everything from the .rav file
+  ##    staticvars = list() ## is anything computed from that; static
   
-  #'READ INPUT DATA AND CALCULATE AEQs
-  #'direct from .rav file or simple calculation from rav file inputs
+  ## 'READ INPUT DATA AND CALCULATE AEQs
+  ## 'direct from .rav file or simple calculation from rav file inputs
   inputs=GetInput(InFile)
   
-  if(NRuns>0) inputs$NRuns=NRuns;
+  # Check if any inputs will be changed
+  newrav=FALSE
+  if(NRuns>0){ inputs$NRuns=NRuns; newrav=TRUE }#NRuns passed in
+  if(NYears>0){ inputs$NYears=NYears; newrav=TRUE } #NYears passed in
+  if(Title != -1){ inputs$Title=NYears; newrav=TRUE } #NYears passed in
+  if(TargetStart>0){ inputs$BufferStart=TargetStart/inputs$TargetU; newrav=TRUE }
+  if(TargetEnd>0){ inputs$BufferEnd=TargetEnd/inputs$TargetU; newrav=TRUE }
+  if(TargetStep>0){ inputs$BufferStep=TargetStep/inputs$TargetU; newrav=TRUE }
+  if(QET>0){ inputs$DL2=QET; newrav=TRUE }
+  if(ERecovery>0){ inputs$ERecovery=ERecovery; newrav=TRUE }
+  if(ECrit>0){ inputs$ECrit=ECrit; newrav=TRUE }
+  #This was computed in GetInput(); need to recompute
+  inputs$BufMax = round((inputs$BufferEnd - inputs$BufferStart) / inputs$BufferStep + 1)
+  #Howard: I doubt this is ok for unix; needed a dir to save the new rav file
+  if(newrav){
+     WriteRavFile(inputs, paste(dirname(InFile),"/",NewRavFileName,sep=""))
+  }
   
-  #add the output file names to the inputs
+  ## add the output file names to the inputs
   inputs = SetOutFileNames(OutFileBase, inputs)
-
+  
   out=RunSims(inputs, silent)
   
-  #'SAVE SUMMARY RESULTS .sum
+  outtm <- proc.time()
+  
+  ## 'SAVE SUMMARY RESULTS .sum
   if(!silent) cat("\nSaving summary...\n")
   SaveSummary(out$inputs, out$SummaryStats, out$staticvars)
   
-  #'SAVE ESCAPEMENT DATA .esc
+  ## 'SAVE ESCAPEMENT DATA .esc
   if(!silent) cat("Saving escapement data...\n")
   SaveEscpmntData(out$inputs, out$SummaryStats)
   
-  #'SAVE BROOD YEAR EXPLOITATION RATE DATA .byr
+  ## 'SAVE BROOD YEAR EXPLOITATION RATE DATA .byr
   if(!silent) cat("Saving BYr year data...\n")
   SaveBYrData(out$inputs, out$SummaryStats)
-
-  return(out)
+  
+  outtm <- proc.time() - outtm
+  
+  if(.Platform$OS.type=="windows") stopCluster(c1)
+  
+  return(c(out,output.time=outtm[3]))
 }

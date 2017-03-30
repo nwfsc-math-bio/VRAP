d026653af66be0d7f106339d4241c4f9ec675334
Eli Holmes <eli.holmes@noaa.gov>
2017-03-30 16:35:32 -0700

    removed for loop to speed up making the byr file

--- R/SaveBYrData.R
+++ R/SaveBYrData.R
@@ -22,16 +22,28 @@ if(inputs$StepFunc == "ER"){
   EBff = Buffer
 }
 
-for(Byr in  -1:(inputs$NYears - inputs$MaxAge)){
-  output=c(output, format(round(inputs$TargetU[inputs$BaseRegime] * EBff,digits=2), nsmall=2,width=6));
-  output=c(output, format(round(inputs$BSRb * PBff),nsmall=0,width=7));
-  output=c(output, format(Byr,nsmall=0,width=7));
-    #plus 2 because R indexing starts at 1
-  output=c(output, format(round(SummaryStats$MinBYrHR[BufNum, Byr+2],digits=3), nsmall=3,width=8));
-  output=c(output, format(round(SummaryStats$AvgBYrHR[BufNum, Byr+2],digits=3), nsmall=3,width=8));
-  output=c(output, format(round(SummaryStats$MaxBYrHR[BufNum, Byr+2],digits=3), nsmall=3,width=8));
-  output=c(output, "\n"); 
-    } #for loop for Byr
+# for(Byr in  -1:(inputs$NYears - inputs$MaxAge)){
+#   output=c(output, format(round(inputs$TargetU[inputs$BaseRegime] * EBff,digits=2), nsmall=2,width=6));
+#   output=c(output, format(round(inputs$BSRb * PBff),nsmall=0,width=7));
+#   output=c(output, format(Byr,nsmall=0,width=7));
+#     #plus 2 because R indexing starts at 1
+#   output=c(output, format(round(SummaryStats$MinBYrHR[BufNum, Byr+2],digits=3), nsmall=3,width=8));
+#   output=c(output, format(round(SummaryStats$AvgBYrHR[BufNum, Byr+2],digits=3), nsmall=3,width=8));
+#   output=c(output, format(round(SummaryStats$MaxBYrHR[BufNum, Byr+2],digits=3), nsmall=3,width=8));
+#   output=c(output, "\n"); 
+#     } #for loop for Byr
+
+Byrs=-1:(inputs$NYears - inputs$MaxAge)
+col1=format(round(inputs$TargetU[inputs$BaseRegime] * EBff,digits=2), nsmall=2,width=6)
+col2=format(round(inputs$BSRb * PBff),nsmall=0,width=7)
+col3=format(Byrs,nsmall=0,width=7)
+col4=format(round(SummaryStats$MinBYrHR[BufNum, Byrs+2],digits=3), nsmall=3,width=8)
+col5=format(round(SummaryStats$AvgBYrHR[BufNum, Byrs+2],digits=3), nsmall=3,width=8)
+col6=format(round(SummaryStats$MaxBYrHR[BufNum, Byrs+2],digits=3), nsmall=3,width=8)
+col7="\n"
+tmp=cbind(col1, col2, col3, col4, col5, col6, col7)
+output=c(output, as.vector(t(tmp)))
+
 } #for loop for Buffer
 cat(file=file, output,sep="")
 }

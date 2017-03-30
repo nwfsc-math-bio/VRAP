2c12118f8967d77ccbeab3151e5775ed3241dc9f
Eli Holmes <eli.holmes@noaa.gov>
2017-03-30 16:35:40 -0700

    removed for loop to speed up making the esc file

--- R/SaveEscpmntData.R
+++ R/SaveEscpmntData.R
@@ -22,16 +22,28 @@ for(Buffer in seq(inputs$BufferStart,inputs$BufferEnd,inputs$BufferStep)){
     EBff = Buffer
   }
   
-for(Year in 1:inputs$NYears){
-  output=c(output, format(round(inputs$TargetU[inputs$BaseRegime] * EBff,digits=2), nsmall=2,width=6));
-  output=c(output, format(round(inputs$BSRb * PBff),nsmall=0,width=7));
-  output=c(output, format(Year,nsmall=0,width=7));
-#plus 2 because R indexing starts at 1
-  output=c(output, format(round(SummaryStats$MinEscpmnt[BufNum, Year],digits=0), nsmall=0,width=8));
-  output=c(output, format(round(SummaryStats$AvgEscpmnt[BufNum, Year],digits=0), nsmall=0,width=8));
-  output=c(output, format(round(SummaryStats$MaxEscpmnt[BufNum, Year],digits=0), nsmall=0,width=8));
-  output=c(output, "\n");  
-} #for loop for year
+# for(Year in 1:inputs$NYears){
+#   output=c(output, format(round(inputs$TargetU[inputs$BaseRegime] * EBff,digits=2), nsmall=2,width=6));
+#   output=c(output, format(round(inputs$BSRb * PBff),nsmall=0,width=7));
+#   output=c(output, format(Year,nsmall=0,width=7));
+# #plus 2 because R indexing starts at 1
+#   output=c(output, format(round(SummaryStats$MinEscpmnt[BufNum, Year],digits=0), nsmall=0,width=8));
+#   output=c(output, format(round(SummaryStats$AvgEscpmnt[BufNum, Year],digits=0), nsmall=0,width=8));
+#   output=c(output, format(round(SummaryStats$MaxEscpmnt[BufNum, Year],digits=0), nsmall=0,width=8));
+#   output=c(output, "\n");  
+# } #for loop for year
+  
+  Years=1:inputs$NYears
+  col1=format(round(inputs$TargetU[inputs$BaseRegime] * EBff,digits=2), nsmall=2,width=6)
+  col2=format(round(inputs$BSRb * PBff),nsmall=0,width=7)
+  col3=format(Years,nsmall=0,width=7)
+  col4=format(round(SummaryStats$MinEscpmnt[BufNum, Years],digits=0), nsmall=0,width=8)
+  col5=format(round(SummaryStats$AvgEscpmnt[BufNum, Years],digits=0), nsmall=0,width=8)
+  col6=format(round(SummaryStats$MaxEscpmnt[BufNum, Years],digits=0), nsmall=0,width=8)
+  col7="\n"
+  tmp=cbind(col1, col2, col3, col4, col5, col6, col7)
+  output=c(output, as.vector(t(tmp)))
+  
 } #for loop for buffer
 cat(file=file, output, sep="")
 }

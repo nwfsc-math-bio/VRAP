9a2d9c3df4d152cea472403bc59ecea70b37d954
Eli Holmes <eli.holmes@noaa.gov>
2018-01-23 11:06:50 -0800

    minor - fix indentation

--- R/SetupSummaryStats.R
+++ R/SetupSummaryStats.R
@@ -1,22 +1,22 @@
 #' @title SetupSummaryStats
-#' @description Create an list set up for all the summary stats. Each list itme is length of BufMax and is all 0.
+#' @description Create an list set up for all the summary stats. Each list is length of BufMax and is all 0.
 #' @param inputs Inputs from .rav file
 #' @return SummaryStats list
 SetupSummaryStats = function(inputs){
   SummaryStats = list()
-
+  
   #most get set to 0 because I add info/NRuns each rep
   #mins and maxs get NA because I use min(a,b,na.rm=TRUE) to ignore initial setting
   SummaryStats[["AvgEscpmnt"]] = matrix(0, inputs$BufMax, inputs$NYears)
-for(el in c("MinEscpmnt","MaxEscpmnt"))
-  SummaryStats[[el]] = matrix(NA, inputs$BufMax, inputs$NYears)
-
+  for(el in c("MinEscpmnt","MaxEscpmnt"))
+    SummaryStats[[el]] = matrix(NA, inputs$BufMax, inputs$NYears)
+  
   SummaryStats[["AvgBYrHR"]] = matrix(0, inputs$BufMax, inputs$NYears - (inputs$MaxAge - inputs$MinAge))
   for(el in c("MinBYrHR","MaxBYrHR"))
     SummaryStats[[el]] = matrix(NA, inputs$BufMax, inputs$NYears - (inputs$MaxAge - inputs$MinAge))
   
-for(el in c("AvgAEQMort","AvgECrit","AvgCaHR", "BufAvgBYrHR", "PropExt", "PropRec", "BufAvgBYrHR", "AveRanFlow", "AveRanMarine"))
-  SummaryStats[[el]] = rep(0, inputs$BufMax)
+  for(el in c("AvgAEQMort","AvgECrit","AvgCaHR", "BufAvgBYrHR", "PropExt", "PropRec", "BufAvgBYrHR", "AveRanFlow", "AveRanMarine"))
+    SummaryStats[[el]] = rep(0, inputs$BufMax)
   
   return(SummaryStats)
 }
